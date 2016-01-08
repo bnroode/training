9c65eee0116059d462a186c5b5ea0c7dcedd3050
Naude Roode <naude@gendac.co.za>
2016-01-08 09:02:02 +0200

    Initial commit

---
+++ Training/Training.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Training.WinPhone.Resources;
+
+namespace Training.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
