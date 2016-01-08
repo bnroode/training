45b1352ef1ab6930d5d6747201f82fc283ee340d
Rudi van Biljon <rudivb@gendac.co.za>
2016-01-08 10:58:12 +0200

    Scan string handling added

--- Training/Training/ViewModel/ScanViewModel.cs
+++ Training/Training/ViewModel/ScanViewModel.cs
@@ -47,6 +47,13 @@ namespace Training.ViewModel
                 message = "Scan cancelled";
             }
             MainText = message;
+            if (result != null && String.Equals(result.BarcodeFormat.ToString(), "PDF_417", StringComparison.OrdinalIgnoreCase))
+            {
+                string[] arr = result.Text.Split('%');
+                string antwoord;
+                antwoord = "Make: " + arr[9] + "\nModel: " + arr[10] + "\nCar Description: " + arr[8] + "\nCar Colour: " + arr[11] + "\nLicense number: " + arr[6] + "\nDate of Expiry: " + arr[14];
+                MainText = antwoord;
+            }
         }
 
         public Command Scan { get; set; }
